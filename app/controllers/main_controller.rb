cf2d067ae34bbc9f82fb4f18b4b6232881551723
Ryan Kulp <ryanckulp@gmail.com>
2020-07-27 15:40:39 +0900

    cleanup, ga

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,4 @@
 class MainController < ApplicationController
-  def index
-  end
-
-  def test
-    render layout: "application"
-  end
-
   def congratulations
     render layout: "install_steps"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ InstallRails::Application.routes.draw do
   resources :install_steps, path: 'steps'
 
   controller :main do
-    get :test
     get :congratulations
   end
 
